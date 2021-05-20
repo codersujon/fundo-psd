8238acd004f92f07c6c1027ab240cd754b5c1be1
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2021-05-20 21:03:50 +0600

    testimonial area done

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,24 @@
         $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
     });
     
+    //Testimonial Slider
+    $('.testimonial-active').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.test-thumb'
+    });
+    $('.test-thumb').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial-active',
+        dots: false,
+        centerMode: true,
+        centerPadding: '0',
+        focusOnSelect: true
+    });
+   
 
 
 })(jQuery)
